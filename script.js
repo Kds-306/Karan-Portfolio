97a4457f0bd2b83754560d4c81f8da066557ce22
Kds-306 <work.karansalunkhe1671@gmail.com>
2025-05-20 14:57:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function loadPage(page) {
+  fetch(page)
+    .then(response => {
+      if (!response.ok) throw new Error("Page not found");
+      return response.text();
+    })
+    .then(html => {
+      document.getElementById("content").innerHTML = html;
+    })
+    .catch(error => {
+      document.getElementById("content").innerHTML = "<p>Page could not be loaded.</p>";
+    });
+}
